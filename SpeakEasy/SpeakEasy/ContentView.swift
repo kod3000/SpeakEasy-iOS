521475c8a899fa899aeed586f7f9147be33d3037
dany <kod3000@gmail.com>
2024-03-19 10:54:39 -0400

    made buttons, setting up todos and organizing tasks

--- SpeakEasy/SpeakEasy/ContentView.swift
+++ SpeakEasy/SpeakEasy/ContentView.swift
@@ -8,30 +8,54 @@
 import SwiftUI
 
 struct SelectLocalFileView: View {
+
     var body: some View {
-        NavigationLink(destination: ListView()) {
-            Text("Select Local File")
+        NavigationLink(destination: LocalFileView()) {
+            Button(title: "Select Local File")
+        
         }
     }
+
 }
 struct SelectUrlView: View {
+    
     var body: some View {
-        NavigationLink(destination: ListView()) {
-            Text("Select URL")
+        NavigationLink(destination: UrlFileView()) {
+            Button(title:"Select URL")
         }
     }
 }
 
 struct LastUsedView: View {
     var body: some View {
-        NavigationLink(destination: ListView()) {
-            Text("See last viewed files")
+        NavigationLink(destination: ListHistoryView()) {
+            Button(title:"See last viewed files")
         }
     }
 }
 
+// Url files
+struct UrlFileView: View {
+    var body: some View {
+            Text("Here you would input a url..")
+    }
+    // TODO: This would call a pdf link
+    // once the link is found it should save it locally
+    // the files should turn up in the local files
+    // this way they have it offline as well.
+}
+
+// Local files
+struct LocalFileView: View {
+    var body: some View {
+            Text("Here you would choose a file..")
+    }
+    // TODO: We need to allow the user to select the
+    // file they want to load int
+}
+
 // ListView to display last used items
-struct ListView: View {
+struct ListHistoryView: View {
     var body: some View {
         List {
             Text("Document PDF 1")
@@ -39,6 +63,10 @@ struct ListView: View {
             Text("Document PDF 3")
         }
     }
+    // TODO: Here we would have a history of
+    // the pdf files the user has played.
+    // history should not repeat the same file
+    
 }
 
 struct ContentView: View {
@@ -46,9 +74,35 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 SelectLocalFileView()
-                LastUsedView() 
+                SelectUrlView()
+                LastUsedView()
             }
             .padding()
+        }
+    }
+    // TODO: Implement logic for url view
+    // TODO: Implement logic for local file view
+    // TODO: Implement logic for history (last used) view
+    
+    // TODO: Missing the logic of loading in the file to
+    // be read.
+    
+    // TODO: Missing the display logic for the text itself
+    
+    // TODO: create backend for regex
+
+}
+
+struct Button: View {
+    var title: String
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color(red: 68 / 255, green: 41 / 255, blue: 182 / 255))
+                .frame(height: 60)
+                .cornerRadius(10)
+                .padding(5)
+            Text(title).foregroundColor(.white)
         }
     }
 }
