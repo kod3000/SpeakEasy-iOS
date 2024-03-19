fc763c6bc69d1420edaf0d58e8c269b50b3f2f01
dany <kod3000@gmail.com>
2024-03-19 05:07:54 -0400

    ..

---
+++ SpeakEasy/SpeakEasy/SpeakEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakEasyApp.swift
+//  SpeakEasy
+//
+//  Created by username on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeakEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
