47a6db98d12d7a1396bc7e9277c09f8348df9f48
dany <kod3000@gmail.com>
2024-03-20 17:47:56 -0400

    wip

--- SpeakEasy/SpeakEasy/Services/PatternLogicFactory.swift
+++ SpeakEasy/SpeakEasy/Services/PatternLogicFactory.swift
@@ -6,6 +6,27 @@
 //
 
 import Foundation
+
+//let fetchedRules: [String: Any] = [
+//    "rules": [
+//        [
+//            "id": 1,
+//            "pattern": "\\[[1-9]+\\*+,[^\\]]*\\]",
+//            "logicName": "removeRefCit"
+//        ],
+//        [
+//            "id": 2,
+//            "pattern": "\\b\\d+(\\.\\d+)*\\b",
+//            "logicName": "sectionFinder"
+//        ],
+//        [
+//            "id": 3,
+//            "pattern": "(\\w+)-(\\n?)(\\w+)",
+//            "logicName": "restoreSplitWords"
+//        ]
+//    ]
+//]
+
 class PatternLogicFactory {
     static func logic(forName name: String) -> ((NSTextCheckingResult, String) -> String)? {
         switch name {
@@ -16,7 +37,17 @@ class PatternLogicFactory {
                 }
                 return originalText
             }
-
+        case "restoreSplitWords":
+            return { match, originalText in
+                if let matchRange = Range(match.range, in: originalText) {
+                    return "\(originalText[matchRange])"
+                }
+                return originalText
+            }
+        case "removeRefCit":
+            return { match, originalText in
+                return ""
+            }
         default:
             return nil
         }
