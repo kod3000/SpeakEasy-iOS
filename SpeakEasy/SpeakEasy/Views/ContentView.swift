5dcd97eab0abf7957dfb17489c185c3362d5e93f
dany <kod3000@gmail.com>
2024-03-22 05:29:34 -0400

    wip

--- SpeakEasy/SpeakEasy/Views/ContentView.swift
+++ SpeakEasy/SpeakEasy/Views/ContentView.swift
@@ -31,7 +31,6 @@ struct ContentView: View {
                 NavigationLink(destination: HistoryView()) {
                          Btn(title:"See last viewed files")
                      }
-                
             }
             .padding()
         }
--- SpeakEasy/SpeakEasy/Views/LocalFilesView.swift
+++ SpeakEasy/SpeakEasy/Views/LocalFilesView.swift
@@ -10,12 +10,47 @@ import SwiftUI
 // TODO: We need to allow the user to select the
 // file they want to load int
 // We need to allow the user to select the file they want to load int
-
-
-struct LocalFilesView: View {
-    var body: some View {
-        Text("Local files ..")
+struct DocumentPicker: UIViewControllerRepresentable {
+    var onDocumentPicked: (URL) -> Void
+        func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(documentTypes: ["com.adobe.pdf"], in: .import)
+        picker.allowsMultipleSelection = false
+        picker.delegate = context.coordinator
+        return picker
     }
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {
+        // Not used in this context
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        var parent: DocumentPicker
+        
+        init(_ documentPicker: DocumentPicker) {
+            self.parent = documentPicker
+        }
+        
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            parent.onDocumentPicked(url)
+        }
+    }
+}
+struct LocalFilesView: View {
+        @State private var showingDocumentPicker = false
+        
+        var body: some View {
+            Button("Select PDF") {
+                showingDocumentPicker = true
+            }
+            .sheet(isPresented: $showingDocumentPicker) {
+                DocumentPicker { url in
+                    print("Selected document at \(url)")
+                    // Here you could do something with the selected PDF file URL
+                }
+            }
+        }
 }
 
 struct LocalFilesView_Previews: PreviewProvider {
