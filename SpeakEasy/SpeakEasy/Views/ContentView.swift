c452fb52d7d39d219378c547f548992e4bf7ecc1
dany <kod3000@gmail.com>
2024-03-23 19:45:17 -0400

    wip

--- SpeakEasy/SpeakEasy/Views/ContentView.swift
+++ SpeakEasy/SpeakEasy/Views/ContentView.swift
@@ -15,34 +15,53 @@ struct ContentView: View {
 
      @State private var isReading = false
      @State private var isLecturing = false
-     
+    @State private var backgroundOffset = CGSize.zero
+
      @ObservedObject private var synthesizerManager = SynthesizerManager()
     
     var body: some View {
+
+        ZStack {
+            // Background
+                      Image("background1")
+                          .resizable()
+                          .aspectRatio(contentMode: .fill)
+                          .offset(x: backgroundOffset.width, y: backgroundOffset.height)
+                          .animation(.linear(duration: 10).repeatForever(autoreverses: true), value: backgroundOffset)
+                          .onAppear {
+                              backgroundOffset = CGSize(width: 20, height: 20)
+                          }
+                          .edgesIgnoringSafeArea(.all)
             VStack {
                 if let pdfURL = pdfURL {
                     PDFContentView(pdfURL: pdfURL, selectedPage: $selectedPage, readFromPage: $readFromPage, isReading: $isReading, isLecturing: $isLecturing, synthesizerManager: synthesizerManager,
-                        onClose: { self.pdfURL = nil }
+                                   onClose: { self.pdfURL = nil }
                     )
                 } else {
                     NavigationView {
-                    VStack {
-                        NavigationLink(destination: FetchUrlView(pdfURL: $pdfURL)) {
-                            Btn(title:"Select URL")
+                        VStack {
+                            NavigationLink(destination: FetchUrlView(pdfURL: $pdfURL)) {
+                                Btn(title:"Select URL")
+                            }
+                            NavigationLink(destination: LocalFilesView(pdfURL: $pdfURL)) {
+                                Btn(title: "Select Local File")
+                            }
+                            NavigationLink(destination: HistoryView(pdfURL: $pdfURL)) {
+                                Btn(title:"See last viewed files")
+                            }
                         }
-                        NavigationLink(destination: LocalFilesView(pdfURL: $pdfURL)) {
-                            Btn(title: "Select Local File")
-                        }
-                        NavigationLink(destination: HistoryView(pdfURL: $pdfURL)) {
-                            Btn(title:"See last viewed files")
-                        }
-                    }
                     }
                 }
             }
             .padding()
+            
+        }
+        .onAppear {
+                    // animate sligthly on appear
+                    backgroundOffset = CGSize(width: -20, height: -20)
+                }
             }
-    // TODO: Implement logic for history (last used) view
+
     // TODO: create backend for regex
 }
 struct ContentView_Previews: PreviewProvider {
